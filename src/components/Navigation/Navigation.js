1d12215f81d59e048d065cc710e6c63458c809b2
jabrielm92 <122121464+jabrielm92@users.noreply.github.com>
2023-03-23 22:56:49 -0400

    Initial commit

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,48 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
-import Logout from "../Logout/Logout"; // Import the Logout component
+import Logout from "../Logout/Logout";
 import "./Navigation.css";
 
 function Navigation() {
   const { currentUser } = useAuth();
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const activeClassName = (isActive) => (isActive ? "active" : "");
 
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <nav className="navigation">
-      {currentUser ? (
-        <>
-          <NavLink to="/" end className={({ isActive }) => activeClassName(isActive)}>
-            Dashboard
-          </NavLink>
-          <NavLink to="/profile" className={({ isActive }) => activeClassName(isActive)}>
-            Profile
-          </NavLink>
-          <NavLink to="/search-exercises" className={({ isActive }) => activeClassName(isActive)}>
-            Search Exercises
-          </NavLink>
-          <NavLink to="/saved-exercises" className={({ isActive }) => activeClassName(isActive)}>
-            Saved Exercises
-          </NavLink>
-          <NavLink to="/workout-plan" className={({ isActive }) => activeClassName(isActive)}>
-            Workout Plan
-          </NavLink>
-          <NavLink to="/progress-tracking" className={({ isActive }) => activeClassName(isActive)}>
-            Progress Tracking
-          </NavLink>
-          <Logout /> {/* Add the Logout component */}
-        </>
-      ) : (
-        <>
-          <NavLink to="/signup" className={({ isActive }) => activeClassName(isActive)}>
-            Sign Up
-          </NavLink>
-          <NavLink to="/login" className={({ isActive }) => activeClassName(isActive)}>
-            Login
-          </NavLink>
-        </>
-      )}
+      <button className="mobile-menu-button" onClick={toggleDropdown}>
+        &#9776;
+      </button>
+      <div className={`nav-links ${showDropdown ? "show" : ""}`}>
+        {currentUser ? (
+          <>
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) => activeClassName(isActive)}
+            >
+              Dashboard
+            </NavLink>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) => activeClassName(isActive)}
+            >
+              Profile
+            </NavLink>
+            <NavLink
+              to="/search-exercises"
+              className={({ isActive }) => activeClassName(isActive)}
+            >
+              Search Exercises
+            </NavLink>
+            <NavLink
+              to="/saved-exercises"
+              className={({ isActive }) => activeClassName(isActive)}
+            >
+              Saved Exercises
+            </NavLink>
+            <NavLink
+              to="/workout-plan"
+              className={({ isActive }) => activeClassName(isActive)}
+            >
+              Workout Plan
+            </NavLink>
+            <NavLink
+              to="/progress-tracking"
+              className={({ isActive }) => activeClassName(isActive)}
+            >
+              Progress Tracking
+            </NavLink>
+            <Logout />
+          </>
+        ) : (
+          <>
+            <NavLink
+              to="/signup"
+              className={({ isActive }) => activeClassName(isActive)}
+            >
+              Sign Up
+            </NavLink>
+            <NavLink
+              to="/login"
+              className={({ isActive }) => activeClassName(isActive)}
+            >
+              Login
+            </NavLink>
+          </>
+        )}
+      </div>
     </nav>
   );
 }
